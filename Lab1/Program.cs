d16cf15a437e2a7be5bc33527e3e90285b18a2bb
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-11-26 15:49:07 -0500

    Committing old repo

---
+++ Lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Grading ID: L8486CIS
+ * LAB 1
+ * CIS199-75
+ * Description: Prints 4 lines of hardcoded text.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Grading ID:      L8486CIS");
+            Console.WriteLine("Hobbies:         Coding and gaming.");
+            Console.WriteLine("Favorite book:   Sandman Slim.");
+            Console.WriteLine("Favorite movie:  Jumper.");
+        }
+    }
+}
